76b74e667e0a335efe4555d337845fbcc9d7a526
schoetlr <srlrs20020@gmail.com>
2016-07-12 18:42:09 -0400

    planning

---
+++ cell.rb
@@ -0,0 +1,5 @@
+class Cell
+
+  #cleared? (whether or not it's data is hidden)
+  #neighbors
+end
---
+++ minesweeper.rb
@@ -0,0 +1,15 @@
+class MineSweeper
+
+  #build the board
+    #instantiate cells
+    #place bombs in cells
+    #find how many neighbors are bombs
+    #assign number
+  
+  #display the board
+    #if check if cell has been cleared (use the Cell#cleared? method)
+    #show number of flags remaining
+
+
+
+end
